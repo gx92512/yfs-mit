8d0b5b4193888c389d1da3d6bcc413955713980e
gaoxiang <gaxi2008@foxmail.com>
2016-10-23 17:47:28 +0800

    solution to lab7

--- lock_smain.cc
+++ lock_smain.cc
@@ -2,19 +2,30 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include "lock_server_cache.h"
+#include <signal.h>
+#include "lock_server_cache_rsm.h"
 #include "paxos.h"
 #include "rsm.h"
+#include <unistd.h>
+#include "lock_server.h"
 
 #include "jsl_log.h"
 
 // Main loop of lock_server
 
+static void
+force_exit(int) {
+    exit(0);
+}
+
 int
 main(int argc, char *argv[])
 {
   int count = 0;
+
+  // Force the lock_server to exit after 20 minutes
+  signal(SIGALRM, force_exit);
+  alarm(20 * 60);
 
   setvbuf(stdout, NULL, _IONBF, 0);
   setvbuf(stderr, NULL, _IONBF, 0);
@@ -38,8 +49,38 @@ main(int argc, char *argv[])
   // RSM layer.
 #define	RSM
 #ifdef RSM
-   rsm rsm(argv[1], argv[2]);
+// You must comment out the next line once you are done with Step One.
+//#define STEP_ONE 
+#ifdef STEP_ONE
+  rpcs server(atoi(argv[1]));
+  lock_server_cache_rsm ls;
+  server.reg(lock_protocol::acquire, &ls, &lock_server_cache_rsm::acquire);
+  server.reg(lock_protocol::release, &ls, &lock_server_cache_rsm::release);
+  server.reg(lock_protocol::stat, &ls, &lock_server_cache_rsm::stat);
+#else
+  rsm rsm(argv[1], argv[2]);
+  lock_server_cache_rsm ls(&rsm);
+  rsm.set_state_transfer((rsm_state_transfer *)&ls);
+  rsm.reg(lock_protocol::acquire, &ls, &lock_server_cache_rsm::acquire);
+  rsm.reg(lock_protocol::release, &ls, &lock_server_cache_rsm::release);
+  rsm.reg(lock_protocol::stat, &ls, &lock_server_cache_rsm::stat);
+#endif // STEP_ONE
 #endif // RSM
+
+#ifndef RSM
+  lock_server_cache ls;
+  rpcs server(atoi(argv[1]), count);
+  server.reg(lock_protocol::stat, &ls, &lock_server_cache::stat);
+  server.reg(lock_protocol::acquire, &ls, &lock_server_cache::acquire);
+  server.reg(lock_protocol::release, &ls, &lock_server_cache::release);
+#endif
+/*#ifndef RSM
+  lock_server ls;
+  rpcs server(atoi(argv[1]), count);
+  server.reg(lock_protocol::stat, &ls, &lock_server::stat);
+  server.reg(lock_protocol::acquire, &ls, &lock_server::acquire);
+  server.reg(lock_protocol::release, &ls, &lock_server::release);
+#endif*/
 
 
   while(1)
